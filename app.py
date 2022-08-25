e4d3fabb108a64cdee0e87b36f30eaf3142ad081
alifha15 <59087878+alifha15@users.noreply.github.com>
2022-08-25 15:21:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,7 @@ import os, random
 from skimage import color, io, filters, morphology
 from tensorflow.keras.models import load_model
 
-list = random.choice(os.listdir("./cascade"))
-plate_cascade = cv2.CascadeClassifier(list)
-
-#plate_cascade = cv2.CascadeClassifier('cascade5.xml')
+plate_cascade = cv2.CascadeClassifier('cascade5.xml')
 MODEL_PATH = 'model31.h5'
 model = load_model(MODEL_PATH,compile=False)
 
